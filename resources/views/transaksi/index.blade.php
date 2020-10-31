d78465c15c46fa866ec9a1dbfe1f177a48585102
novilfahlevy <novilfreon@gmail.com>
2020-10-31 14:52:07 +0800

    export transaksi

--- resources/views/transaksi/index.blade.php
+++ resources/views/transaksi/index.blade.php
@@ -88,7 +88,7 @@ Transaksi Penjualan
         <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title">Tambah Lokasi</h5>
+                <h5 class="modal-title">Export Data Transaksi</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
