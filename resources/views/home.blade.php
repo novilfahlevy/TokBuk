e4d84fa8696464f147f48d2d71a3ca0ee59dc4bd
novilfahlevy <novilfreon@gmail.com>
2020-10-31 13:49:38 +0800

    transaksi icon dasbor

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,7 +59,7 @@
               <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                   <div class="card-icon bg-success">
-                    <i class="fas fa-circle"></i>
+                    <i class="fas fa-money-bill"></i>
                   </div>
                   <div class="card-wrap">
                     <div class="card-header">
