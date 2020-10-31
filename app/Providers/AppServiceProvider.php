2a5ead7eb52aa7bf199b09b3e5418b378b6809b2
novilfahlevy <novilfreon@gmail.com>
2020-10-31 16:17:52 +0800

    set carbon format dan timezone asia/makassar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Carbon::setLocale(config('app.locale'));
+        Carbon::setToStringFormat('d-m-Y H:i');
     }
 }
