b9fb2b12a0d3473c69c90dd3c61e6e5e7e47977c
unknown <alisanabela25@gmail.com>
2020-11-03 07:21:13 +0800

    forgot password

--- config/mail.php
+++ config/mail.php
@@ -83,10 +83,11 @@ return [
     |
     */
 
-    'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
-    ],
+    // 'from' => [
+    //     'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+    //     'name' => env('MAIL_FROM_NAME', 'Example'),
+    // ],
+    'from' => ['address' => 'tokbuk3@gmail.com', 'name' => 'TokBuk'],
 
     /*
     |--------------------------------------------------------------------------
--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
+    'reset' => 'Password anda telah direset!',
+    'sent' => 'Kami telah mengirim link reset password!',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
