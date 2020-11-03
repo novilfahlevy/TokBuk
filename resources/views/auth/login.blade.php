2c3b9d418f251f732677ef1ffff2e629b17b8c05
novilfahlevy <novilfreon@gmail.com>
2020-11-03 19:22:18 +0800

    login logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,8 +25,8 @@
       <div class="container mt-5">
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-            <div class="login-brand mb-3">
-              <img src="{{ asset('assets/img/logo.png') }}" alt="logo" style="width: 11em" height="120">
+            <div class="login-brand mb-4">
+              <img src="{{ asset('assets/img/logo.png') }}" alt="logo" style="width: 9.4em" height="90">
             </div>
 
             <div class="card">
