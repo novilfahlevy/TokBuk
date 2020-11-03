239af4f8d5332de8908804cec8cdc1d4ebdd0cf6
unknown <alisanabela25@gmail.com>
2020-11-03 15:24:25 +0800

    seeder penulis, penerbit, pemasok, buku, user

--- database/seeders/BukuTableSeeder.php
+++ database/seeders/BukuTableSeeder.php
@@ -15,25 +15,80 @@ class BukuTableSeeder extends Seeder
             'sampul' => 'sampul.png',
             'isbn' => '9786022202417',
             'judul' => 'Janshen',
-            'tahun_terbit' => 2015,
+            'tahun_terbit' => 2017,
             'id_penulis' => 1,
             'id_penerbit' => 2,
-            'id_kategori' => 3,
+            'id_kategori' => 1,
             'id_lokasi' => 1,
             'harga' => 80000,
-            'jumlah' => 80
+            'jumlah' => 30
         ]);
+
         App\Buku::create([
             'sampul' => 'sampul.png',
             'isbn' => '979103504504',
-            'judul' => 'RPUL',
-            'tahun_terbit' => 2017,
-            'id_penulis' => 2,
+            'judul' => 'RPUL Edisi Terbaru',
+            'tahun_terbit' => 2019,
+            'id_penulis' => 3,
+            'id_penerbit' => 4,
+            'id_kategori' => 3,
+            'id_lokasi' => 4,
+            'harga' => 30000,
+            'jumlah' => 35
+        ]);
+
+        App\Buku::create([
+            'sampul' => 'sampul.png',
+            'isbn' => '9786021286746',
+            'judul' => 'Akutansi Entitas Manufaktur',
+            'tahun_terbit' => 2019,
+            'id_penulis' => 4,
             'id_penerbit' => 1,
             'id_kategori' => 3,
+            'id_lokasi' => 4,
+            'harga' => 90000,
+            'jumlah' => 40
+        ]);
+
+        App\Buku::create([
+            'sampul' => 'sampul.png',
+            'isbn' => '9792056440',
+            'judul' => 'Doraemon Canda',
+            'tahun_terbit' => 2004,
+            'id_penulis' => 5,
+            'id_penerbit' => 3,
+            'id_kategori' => 2,
+            'id_lokasi' => 3,
+            'harga' => 15000,
+            'jumlah' => 35
+        ]);
+
+        App\Buku::create([
+            'sampul' => 'sampul.png',
+            'isbn' => '9789797809034',
+            'judul' => 'Rentang Kisah',
+            'tahun_terbit' => 2017,
+            'id_penulis' => 6,
+            'id_penerbit' => 5,
+            'id_kategori' => 1,
             'id_lokasi' => 1,
             'harga' => 85000,
-            'jumlah' => 76
+            'jumlah' => 30
         ]);
+
+        App\Buku::create([
+            'sampul' => 'sampul.png',
+            'isbn' => '9786022201502',
+            'judul' => 'Danur gerbang dialog',
+            'tahun_terbit' => 2015,
+            'id_penulis' => 1,
+            'id_penerbit' => 2,
+            'id_kategori' => 1,
+            'id_lokasi' => 1,
+            'harga' => 90000,
+            'jumlah' => 20
+        ]);
+
+        
     }
 }
--- database/seeders/PemasokTableSeeder.php
+++ database/seeders/PemasokTableSeeder.php
@@ -12,22 +12,47 @@ class PemasokTableSeeder extends Seeder
     public function run()
     {
         App\Pemasok::create([
-            'nama' => 'Ahmad',
+            'nama' => 'Sukses Sejahtera',
             'alamat' => 'Jl. Anggrek No.08',
-            'email' => 'ahmad@gmail.com',
+            'email' => 'suksessejahtera@gmail.com',
             'telepon' => '085689785678'
         ]);
+
         App\Pemasok::create([
-            'nama' => 'Dimas',
+            'nama' => 'Alam Sejahtera',
             'alamat' => 'Jl. Melati No.09',
-            'email' => 'dimas@gmail.com',
+            'email' => 'Alamsejahtera@gmail.com',
             'telepon' => '089678998899'
         ]);
+
         App\Pemasok::create([
-            'nama' => 'Rara',
+            'nama' => 'Sinar Kemboja',
             'alamat' => 'Jl. Kenanga No.11',
-            'email' => 'rara@gmail.com',
+            'email' => 'sinark@gmail.com',
             'telepon' => '084567896767'
         ]);
+
+        App\Pemasok::create([
+            'nama' => 'Aktiva Makmur',
+            'alamat' => 'Jl. Cempaka No.14',
+            'email' => 'aktivamakmur@gmail.com',
+            'telepon' => '084578785656'
+        ]);
+
+        App\Pemasok::create([
+            'nama' => 'Tuan Agung',
+            'alamat' => 'Jl. Kamboja No.67',
+            'email' => 'tuanagung@gmail.com',
+            'telepon' => '08457856454545'
+        ]);
+
+        App\Pemasok::create([
+            'nama' => 'Adhiguna Mandiri',
+            'alamat' => 'Jl. Pahlawan No.1',
+            'email' => 'adhigunaman@gmail.com',
+            'telepon' => '085245456767'
+        ]);
+
+
     }
 }
