4759a1a46a4b146b01ee6c0ed223f33a2648f3dc
novilfahlevy <novilfreon@gmail.com>
2020-11-04 16:26:11 +0800

    hilangkan label harga pada pilihan pembelian buku

--- public/js/pembelian_buku/create.js
+++ public/js/pembelian_buku/create.js
@@ -29,10 +29,7 @@ function fillJsSelect2Options(selectClass, initCallback) {
         return $(`
           <span class="d-flex align-items-center">
             <img src="${BASEURL}/images/buku/${buku.sampul}" width="50" height="50" class="mr-3" />
-            <div class="d-flex flex-column">
-              ${buku.judul.slice(0, 28)}${buku.judul.length > 28 ? '...' : ''}
-              <span>Rp ${format(buku.harga)}</span>
-            </div>
+            ${buku.judul.slice(0, 30)}${buku.judul.length > 30 ? '...' : ''}
           </span>
         `);
       }
