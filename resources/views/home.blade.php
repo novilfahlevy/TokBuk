02b214fcd5c4faaa12dab683415f2cf2ac127697
novilfahlevy <novilfreon@gmail.com>
2020-11-05 09:05:52 +0800

    hapus source code chart dasbor

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -157,7 +157,3 @@
 @push('js')
   <script src="{{ asset('js/dasbor/index.js') }}"></script>
 @endpush
-
-@push('css')
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css" integrity="sha512-/zs32ZEJh+/EO2N1b0PEdoA10JkdC3zJ8L5FTiQu82LR9S/rOQNfQN7U59U9BC12swNeRAz3HSzIL2vpp4fv3w==" crossorigin="anonymous" />
-@endpush
