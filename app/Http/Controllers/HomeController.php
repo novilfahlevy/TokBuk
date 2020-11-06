d790b67e54cf1807b1b11366114dab5eec32219c
novilfahlevy <novilfreon@gmail.com>
2020-11-06 12:44:02 +0800

    fix dasbor chart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,15 +32,32 @@ class HomeController extends Controller
     public function index()
     {
         $pengguna = User::count();
-				$judulBuku = Buku::count();
-				$buku = (int) Buku::sum('jumlah');
-				$transaksi = Transaksi::whereDate('created_at', $this->now->today()->format('Y-m-d'))->count();
-				$chartTransaksi = Transaksi::select(db::raw('SUM(total_harga) AS total_perbulan'))
-					->whereYear('created_at', $this->now->year)
-					->groupBy(DB::raw('MONTH(created_at)'))
-					->get()
-					->map(function($transaksi) { return $transaksi->total_perbulan; })
-					->toArray();
+        $judulBuku = Buku::count();
+        $buku = (int) Buku::sum('jumlah');
+        $transaksi = Transaksi::whereDate('created_at', $this->now->today()->format('Y-m-d'))->count();
+        $chartTransaksi = Transaksi::select([
+            DB::raw('SUM(total_harga) AS total_perbulan'),
+            DB::raw('MONTH(created_at) AS bulan')
+        ])
+        ->whereYear('created_at', $this->now->year)
+        ->groupBy([
+            DB::raw('MONTH(created_at)'),
+            'bulan'
+        ])
+        ->get();
+
+        $chartTransaksi = collect([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+            ->map(function($bulan) use ($chartTransaksi) {
+                return $chartTransaksi->map(function($transaksi) use ($bulan) {
+                    return [
+                        'bulan' => $transaksi->bulan == $bulan ? $transaksi->bulan : $bulan,
+                        'total_perbulan' => $transaksi->bulan == $bulan ? $transaksi->total_perbulan : 0
+                    ];
+                });
+            })
+            ->map(function($transaksi) {
+                return $transaksi[0]['total_perbulan'];
+            });
         
         return view('home', compact('pengguna', 'judulBuku', 'buku', 'transaksi', 'chartTransaksi'));
     }
