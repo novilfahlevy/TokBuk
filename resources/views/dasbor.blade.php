3f96f733768c8edac773869183b674c09789b26e
novilfahlevy <novilfreon@gmail.com>
2020-11-21 10:05:59 +0800

    fix aktivitas

--- resources/views/dasbor.blade.php
+++ resources/views/dasbor.blade.php
@@ -71,12 +71,14 @@
                   </div>
                 </div>
               </div>
-              <div class="col-12">
-                <div class="alert alert-info mb-4" role="alert">
-                  <h5 class="alert-heading">Aktivitas Terakhir</h5>
-                  <p><b>{{ $aktivitasTerakhir->user->name }}</b> baru saja "<b>{{ $aktivitasTerakhir->aktivitas }}</b>" pada <b>{{ $aktivitasTerakhir->created_at }}</b> atau {{ $aktivitasTerakhir->created_at->diffForHumans() }}.</p>
+              @if ( $aktivitasTerakhir )
+                <div class="col-12">
+                  <div class="alert alert-info mb-4" role="alert">
+                    <h5 class="alert-heading">Aktivitas Terakhir</h5>
+                    <p><b>{{ $aktivitasTerakhir->user->name }}</b> baru saja "<b>{{ $aktivitasTerakhir->aktivitas }}</b>" pada <b>{{ $aktivitasTerakhir->created_at }}</b> atau {{ $aktivitasTerakhir->created_at->diffForHumans() }}.</p>
+                  </div>
                 </div>
-              </div>
+              @endif
               <div class="col-6">
                 <div class="card">
                   <div class="card-header">
